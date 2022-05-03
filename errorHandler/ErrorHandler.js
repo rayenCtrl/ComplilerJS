0b7e6f8265a653ab004b8c986a01f1fb526393f5
rayen <raynlazghab@gmail.com>
2022-05-03 18:21:20 +0100

    Adding line nbre to error log

---
+++ errorHandler/ErrorHandler.js
@@ -0,0 +1,26 @@
+const { globalLog } = require('../log/Log')
+
+class ErrorHandler {
+  constructor() {
+    this.log = globalLog
+  }
+
+  throw(msg, line, col) {
+    let err
+    if (line && col) {
+      err = `\n\n${msg} -- ln: ${line}, col: ${col}\n`
+    } else {
+      err = `\n\n${msg}\n`
+    }
+    try {
+      throw new Error(err)
+    } catch (e) {
+      console.error(e)
+      this.log.error(e, line)
+    }
+  }
+}
+
+module.exports = {
+  ErrorHandler,
+}
